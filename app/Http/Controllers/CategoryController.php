11b6106f5752726059b9564206ad5cd7acc2fcfa
annfont0 <fontanillajasmine00@gmail.com>
2023-11-09 14:30:47 +0800

    Remove unnecessary imports

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,10 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Carbon\Carbon;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
 
 class CategoryController extends Controller
 {
